12b936fa580a7d9ee999190f5fb6bd42df57a436
Dylan <dylancalaflatham@gmail.com>
2024-03-17 21:56:45 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -20,54 +20,25 @@ namespace ns {
             {
                 RUNTIME_OR_RELEASE_GUARD
 
-				const auto file_to_open = "res://Hello.txt";
+				const auto file_to_open = "res://file-access/Hello.txt";
 
 				//---
-				//(A) FileAccess static method call
+                //(A) FileAccess file_exists static method call
+                //---
+
+                if (godot::FileAccess::file_exists(file_to_open))
+                    godot::UtilityFunctions::print("(A) godot::FileAccess returned existing file.");
+                else
+                    godot::UtilityFunctions::print("(A) godot::FileAccess returned NON existing file.");
+
+				//---
+				//(B) FileAccess open static method call
                 //---
 
                 if (godot::FileAccess::open(file_to_open, godot::FileAccess::READ).is_null())
-                    godot::UtilityFunctions::print("(A) godot::FileAccess open static method call is null.");
+                    godot::UtilityFunctions::print("(B) godot::FileAccess open static method call is null.");
 				else
-                    godot::UtilityFunctions::print("(A) godot::FileAccess open static method call returned FileAccess successfully.");
-
-				//---
-				//(B) FileAccess stack object calls
-                //---
-
-                godot::FileAccess file_access_stack;
-                const auto file_access_B = file_access_stack.open(file_to_open, godot::FileAccess::READ);
-                if (file_access_B.is_null())
-                {
-                    godot::UtilityFunctions::print("(B) Stack file access static method call is null.");
-                }
-                else
-                {
-                    godot::UtilityFunctions::print("(B) Stack file access open static method call returned FileAccess successfully.");
-                }
-
-				//---
-				//(C) FileAccess ref counted instance calls
-                //---
-
-                //const godot::Ref file_access_ref(memnew(godot::FileAccess));
-                //if (file_access_ref == nullptr)
-                //{
-                //    godot::UtilityFunctions::print("(C) Ref fileaccess is nullptr.");
-                //}
-                //else
-                //{
-                //    godot::UtilityFunctions::print("(C) Ref fileaccess initialization successful.");
-                //    auto file_access = file_access_ref->open(file_to_open, godot::FileAccess::READ);
-                //    if (file_access == nullptr)
-                //    {
-                //        godot::UtilityFunctions::print("(C) Ref fileaccess is nullptr.");
-                //    }
-                //    else
-                //    {
-                //        godot::UtilityFunctions::print("(C) Ref fileaccess initialization successful.");
-                //    }
-                //}
+                    godot::UtilityFunctions::print("(B) godot::FileAccess open static method call returned FileAccess successfully.");
             }
             break;
         }
