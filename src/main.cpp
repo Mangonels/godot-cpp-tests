8545931a6c9e3b44b68c4cb9405819826b3933a4
Mangonels <dylancalaflatham@gmail.com>
2025-01-14 17:12:35 +0100

    testing std::thread in a godot node

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,43 @@ namespace ns {
         {
             case NOTIFICATION_READY:
             {
-                // set_process(true);
-                // set_physics_process(true);
-                // set_process_mode(PROCESS_MODE_PAUSABLE);
+                set_process(true);
 
-                godot::UtilityFunctions::print("Hello World!");
+                run_thread_loop = true;
+                test_thread = std::thread(&Main::thread_function, this);
+            }
+            case NOTIFICATION_PROCESS:
+            {
+                godot::UtilityFunctions::print("Main Thread!");
+            }
+            case NOTIFICATION_WM_CLOSE_REQUEST:
+            {
+                run_thread_loop = false;
+                // Adding the following join for the extra thread
+                // causes a crash on game start:
+                test_thread.join();
+				// I also tried adding it in the destructor.
             }
             break;
         }
+    }
+
+    void Main::thread_function()
+    {
+        /* NOTE: my computer runs 1702 loop iterations from this separate thread 
+        between each main thread PROCESS notification, so godot's NOTIFICATION_PROCESS
+        (which is the "highest frequency loop" for Godot that I know of)
+        is much less frequent.
+
+		It was actually possible to run a print from utility functions at this frequency,
+		but it's unconfirmed what other Godot Engine API functions may be runnable.
+
+        There are also some limitations as to which API functionalities can be called
+        from an extra thread.
+
+        - PixelRain */
+
+        while (run_thread_loop)
+            godot::UtilityFunctions::print("Extra Thread!"); 
     }
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <thread>
+
 #include <godot_cpp/classes/node.hpp>
 
 namespace ns {
@@ -11,8 +13,12 @@ namespace ns {
         Main() = default;
         ~Main() = default;
 
+        bool run_thread_loop = false;
+        std::thread test_thread;
+
     protected:
         void _notification(int p_notification);
+        void thread_function();
 
         static void _bind_methods()
         {
